429071a1bc99052ba3a1e700d8a4b26196388ce0
mi-1000 <119438442+mi-1000@users.noreply.github.com>
2026-06-04 16:58:03 +0200

    init project: added scripts to preprocess WIF/StackExchange corpora

---
+++ README.md
@@ -0,0 +1,5 @@
+Contains scripts to aggregate and format data for synthetic language conversational data generation.
+
+Sources:
+    - WIF Corpus -- "The written production in learner French (WIF) corpus: a new resource for examining children’s writing" (Lindqvist, 2026)
+    - French Stack Exchange (https://french.stackexchange.com) (last free-access data dump from June 2024) -- Data was downloaded from https://archive.org/download/stackexchange (french.stackexchange.com.7z) on the 2nd of June 2026 @ 14h CEST (CC-BY-SA 4.0 License)
---
+++ scripts/build_dataset_stackexchange.py
@@ -0,0 +1,61 @@
+import xml.etree.ElementTree as ET
+import json
+import os
+import regex
+
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+
+INPUT_FILE = os.path.join("..", "data", "StackExchange", "Posts.xml")
+OUTPUT_FILE = os.path.join("..", "data", "StackExchange", "french_dataset.jsonl")
+
+def parse_html(html_content: str) -> str:
+    """Parse HTML content and return clean text."""
+    if not html_content:
+        return ""
+    soup = BeautifulSoup(html_content, "html.parser")
+    text = soup.get_text()
+    
+    # Normalize newlines
+    text = text.replace("\u2028", "\n").replace("\u2029", "\n")
+    text = text.replace("\r\n", "\n").replace("\r", "\n")
+    
+    # Remove Unicode characters that are not meaningful for the dataset
+    text = regex.sub(r'(?>[\p{Cf}\p{Cc}](?<!\n))+', '', text) # Remove invisible control characters, except for newlines
+    text = regex.sub(r'\p{Z}', ' ', text) # Replace all kinds of whitespace with a single space
+    
+    return text
+
+def build_dataset() -> None:
+    with open(OUTPUT_FILE, "w", encoding="utf-8") as f_out:
+        n_lines  = 0
+        for _event, elem in tqdm(ET.iterparse(INPUT_FILE, events=("end",)), desc="Processing posts"):
+            if elem.tag == "row":
+                post_type = elem.get("PostTypeId")
+                score = int(elem.get("Score", 0))
+
+                if (post_type == "1" and score >= 2): # Questions (PostTypeId="1") with score >= 2 to ensure minimal relevance
+                    question_id = elem.get("Id")
+                    title = elem.get("Title")
+                    body = elem.get("Body")
+                    tags = elem.get("Tags", "").strip('|').split('|')
+
+                    title = parse_html(title)
+                    body = parse_html(body)
+
+                    data = {
+                        "id": question_id,
+                        "question": title,
+                        "body": body,
+                        "score": score,
+                        "tags": tags,
+                    }
+                    f_out.write(json.dumps(data, ensure_ascii=False, indent=2) + "\n")
+                    n_lines += 1
+
+                elem.clear() # Manage memory
+
+    print(f"Succesfully saved {n_lines} lines in {OUTPUT_FILE}.")
+
+if __name__ == "__main__":
+    build_dataset()
